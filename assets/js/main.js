2631fbc4752a404e5761f0a672d12b807ef91dbf
dev-vxr <102435225+dev-vxr@users.noreply.github.com>
2022-07-16 18:09:53 +0100

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+//sticky navbar
+
+let nav = document.querySelector('.nav-active');
+
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 10) {
+    nav.classList.add('active-scroll');
+  } else {
+    nav.classList.remove('active-scroll');
+  }
+}
+
+
+//scroll reveal
+
+const scroll = ScrollReveal();
+
+scroll.reveal('.home-col, .social', {
+  origin: 'bottom',
+  distance: '50px',
+  duration: 2500,
+  interval: 500,
+  scale: 1
+})
+
+scroll.reveal('.about-col', {
+  origin: 'left',
+  distance: '50px',
+  duration: 1000,
+  interval: 500,
+  scale: 1
+})
+
+scroll.reveal('.left', {
+  origin: 'left',
+  distance: '25px',
+  duration: 1000,
+  interval: 500,
+  scale: 1
+})
+
+scroll.reveal('.right', {
+  origin: 'right',
+  distance: '25px',
+  duration: 1000,
+  interval: 500,
+  scale: 1
+})
